617fbf3703ae372e05a14a2906c1b1564ecf1ba4
CavemanUK <101810188+CavemanUK@users.noreply.github.com>
2025-06-06 11:52:23 +0100

    Fix BaseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://cavemanuk.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/AimBob-docs/',
+  baseUrl: '/AimBob-Docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
